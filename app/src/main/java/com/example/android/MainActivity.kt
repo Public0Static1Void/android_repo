4104922050b688fa12dd3777e8c6ead2f48a02f8
Dani <dangp2003@gmail.com>
2023-12-28 17:10:14 +0100

    0.2

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.android// com.example.android.MainActivity.kt
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.R
 
@@ -7,5 +12,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_screen)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.start_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            title = null // Hace que no aparezca el título del proyecto en la toolbar
+        }
+
+        val menu_button: ImageView = findViewById(R.id.btn_menu)
+        menu_button.setOnClickListener { view ->
+            showPopupMenu(view)
+        }
+    }
+
+    private fun showPopupMenu(view:View){
+        val popupMenu = PopupMenu(this,view)
+        popupMenu.inflate(R.menu.menu_settings)
+
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.action_settings -> {
+                    // Lógica cuando se selecciona la opción del menú
+                    Toast.makeText(this, "Configuración seleccionada", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                // Otros casos si hay más elementos en el menú
+                else -> false
+            }
+        }
+
+        popupMenu.show()
     }
 }
