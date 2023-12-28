4f3de2fb182cb5a09b0ce989a66f79ad2107cbfe
Dani <dangp2003@gmail.com>
2023-12-28 20:29:57 +0100

    0.4

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,22 +1,41 @@
 package com.example.android// com.example.android.MainActivity.kt
+
 import android.os.Bundle
 import android.view.View
-import android.view.View.OnLayoutChangeListener
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.PopupMenu
+import android.widget.SearchView
+import android.widget.TextView
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import com.example.android.R
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface RiotGamesAPI {
+    @GET("/lol/summoner/v4/summoners/by-name/{summonerName}")
+    fun getSummonerByName(
+        @Path("summonerName") summonerName: String,
+        @Query("api_key") apiKey: String // Parámetro para la clave de API
+    ): Call<Summoner>
+    // Define más llamadas a la API según tus necesidades
+}
+
 
 class MainActivity : AppCompatActivity() {
 
-    val menu_button: ImageView by lazy { findViewById<ImageView>(R.id.btn_menu) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_screen)
 
+
+/*
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.start_toolbar)
         setSupportActionBar(toolbar)
 
@@ -28,19 +47,49 @@ class MainActivity : AppCompatActivity() {
         menu_button.setOnClickListener { view ->
             showPopupMenu(view)
         }
+
+ */
+
+        RestoreMenuButtons()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putBoolean("buttonEnabled", menu_button.isEnabled)
-        super.onSaveInstanceState(outState)
-    }
+    private fun SetProfile(nombre: String){
+        setContentView(R.layout.profile_layout)
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val isButtonEnabled = savedInstanceState.getBoolean("buttonEnabled")
-        menu_button.isEnabled = isButtonEnabled
-    }
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/") // URL base de la API de Riot Games
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
+        val riotGamesAPI = retrofit.create(RiotGamesAPI::class.java)
+
+        val text: TextView = findViewById(R.id.user_name)
+        val level: TextView = findViewById(R.id.user_level)
+
+        val call = riotGamesAPI.getSummonerByName(nombre, "RGAPI-9df185aa-7420-4cba-b1b8-c8aaf29ae216")
+
+        call.enqueue(object : Callback<Summoner> {
+            override fun onResponse(call: Call<Summoner>, response: Response<Summoner>) {
+
+                if (response.isSuccessful) {
+                    val summoner = response.body()
+                    // Haz algo con los datos del invocador aquí
+                    if (summoner != null) {
+                        text.text = summoner.name
+                        level.text = summoner.summonerLevel
+                    }
+                } else {
+                    // Manejar errores si la solicitud no fue exitosa
+                    text.text= "Usuario no encontrado"
+                }
+            }
+
+            override fun onFailure(call: Call<Summoner>, t: Throwable) {
+                // Manejar errores de conexión u otros errores aquí
+                text.text= "c"
+            }
+        })
+    }
 
     private fun showPopupMenu(view: View) {
         val popupMenu = PopupMenu(this, view)
@@ -68,5 +117,33 @@ class MainActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         setContentView(R.layout.start_screen)
+        RestoreMenuButtons()
+    }
+
+    private fun RestoreMenuButtons(){
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.start_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            title = null // Hace que no aparezca el título del proyecto en la toolbar
+        }
+
+        val menu_button: ImageView = findViewById(R.id.btn_menu)
+        menu_button.setOnClickListener { view ->
+            showPopupMenu(view)
+        }
+
+        val menu_search: SearchView = findViewById(R.id.search_view)
+        menu_search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // Realizar la búsqueda cuando se presiona Enter o se confirma la búsqueda
+                SetProfile(query)
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
     }
 }
